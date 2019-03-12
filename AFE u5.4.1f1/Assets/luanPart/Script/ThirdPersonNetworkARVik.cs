568fa47053cb5ba87b0a872e08dd3b1fe2442611
DESKTOP-5D9G5PK\Asrock <thandh@beetsoft.com.vn>
2019-03-12 15:27:05 +0700

    use async scale by transform, not by component

--- AFE u5.4.1f1/Assets/luanPart/Script/ThirdPersonNetworkARVik.cs
+++ AFE u5.4.1f1/Assets/luanPart/Script/ThirdPersonNetworkARVik.cs
@@ -43,24 +43,21 @@ public class ThirdPersonNetworkARVik : Photon.MonoBehaviour
         {
             //Network player, receive data            
             correctPlayerPos = (Vector3)stream.ReceiveNext();
-            correctPlayerRot = (Quaternion)stream.ReceiveNext();
-            correctPlayerScale = (Vector3)stream.ReceiveNext();
+            correctPlayerRot = (Quaternion)stream.ReceiveNext();            
             GetComponent<Rigidbody>().velocity = (Vector3)stream.ReceiveNext();
 
             if (!appliedInitialUpdate)
             {
                 appliedInitialUpdate = true;
                 transform.position = correctPlayerPos;
-                transform.rotation = correctPlayerRot;
-                transform.localScale = correctPlayerScale;
+                transform.rotation = correctPlayerRot;                
                 GetComponent<Rigidbody>().velocity = Vector3.zero;
             }
         }
     }
 
     private Vector3 correctPlayerPos = Vector3.zero; //We lerp towards this
-    private Quaternion correctPlayerRot = Quaternion.identity; //We lerp towards this
-    private Vector3 correctPlayerScale = Vector3.zero; //We lerp towards this
+    private Quaternion correctPlayerRot = Quaternion.identity; //We lerp towards this    
 
     void Update()
     {
@@ -68,8 +65,7 @@ public class ThirdPersonNetworkARVik : Photon.MonoBehaviour
         {
             //Update remote player (smooth this, this looks good, at the cost of some accuracy)
             transform.position = Vector3.Lerp(transform.position, correctPlayerPos, Time.deltaTime * 5);
-            transform.rotation = Quaternion.Lerp(transform.rotation, correctPlayerRot, Time.deltaTime * 5);            
-            transform.localScale = Vector3.Lerp(transform.localScale, correctPlayerScale, Time.deltaTime * 5);
+            transform.rotation = Quaternion.Lerp(transform.rotation, correctPlayerRot, Time.deltaTime * 5);                        
         }
     }
 
