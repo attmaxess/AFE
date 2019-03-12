546a0898ec4e5dc787e26f01b8e86a68692ae26b
DESKTOP-5D9G5PK\Asrock <thandh@beetsoft.com.vn>
2019-03-12 16:00:29 +0700

    remove standard asset

--- AFE u5.4.1f1/Assets/luanPart/Script/ThirdPersonControllerARVik.cs
+++ AFE u5.4.1f1/Assets/luanPart/Script/ThirdPersonControllerARVik.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using UnityEngine;
-using UnityStandardAssets;
-using UnityStandardAssets.CrossPlatformInput;
 
 public class ThirdPersonControllerARVik : MonoBehaviour, ICharacterTranform
 {
