d53c25b95ff374f31f3fe0265323a673cfbf0bea
luanntbeetsoft <luannt@beetsoft.com.vn>
2019-03-13 15:01:46 +0700

    new joystick

--- AFE u5.4.1f1/Assets/luanPart/Script/GameManagerArVik.cs
+++ AFE u5.4.1f1/Assets/luanPart/Script/GameManagerArVik.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using ControlFreak2;
 
 public class GameManagerArVik : Photon.PunBehaviour
 {
@@ -23,7 +24,7 @@ public class GameManagerArVik : Photon.PunBehaviour
 
     private void Awake()
     {
-        
+
     }
 
     IEnumerator OnLeftRoom()
