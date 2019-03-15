1dc36400c887146749a2565d04c921e085de6999
DESKTOP-5D9G5PK\Asrock <thandh@beetsoft.com.vn>
2019-03-15 10:42:39 +0700

    set world origin

--- AFE u5.4.1f1/Assets/UnityARKitPlugin/Examples/ARKit1.5/UnityARSetWorldOrigin/SetWorldOriginControl.cs
+++ AFE u5.4.1f1/Assets/UnityARKitPlugin/Examples/ARKit1.5/UnityARSetWorldOrigin/SetWorldOriginControl.cs
@@ -4,21 +4,22 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.XR.iOS;
 
-public class SetWorldOriginControl : MonoBehaviour {
+public class SetWorldOriginControl : MonoBehaviour
+{
+    public Camera arCamera;
+    public Text positionText;
+    public Text rotationText;
 
-	public Camera arCamera;
-	public Text positionText;
-	public Text rotationText;
 
+    // Update is called once per frame
+    void Update()
+    {
+        positionText.text = "Camera position=" + arCamera.transform.position.ToString();
+        rotationText.text = "Camera rotation=" + arCamera.transform.rotation.ToString();
+    }
 
-	// Update is called once per frame
-	void Update () {
-		positionText.text = "Camera position=" + arCamera.transform.position.ToString ();
-		rotationText.text = "Camera rotation=" + arCamera.transform.rotation.ToString ();
-	}
-
-	public void SetWorldOrigin()
-	{
-		UnityARSessionNativeInterface.GetARSessionNativeInterface().SetWorldOrigin (arCamera.transform);
-	}
+    public void SetWorldOrigin()
+    {
+        UnityARSessionNativeInterface.GetARSessionNativeInterface().SetWorldOrigin(arCamera.transform);
+    }
 }
---
+++ AFE u5.4.1f1/Assets/tPart/SetWorldOrigins/Scripts/SliderChangeIpf.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderChangeIpf : MonoBehaviour
+{
+    public InputField ipf = null;
+    public Slider slider = null;
+
+    [ContextMenu("OnChangeValue")]
+    public void OnChangeValue()
+    {
+        ipf.text = slider.value.ToString();
+    }
+}
