84de11e842bbe58c091a6297b48bb88397b9e60a
luanntbeetsoft <luannt@beetsoft.com.vn>
2019-03-21 16:31:32 +0700

    no message

--- AFE u5.4.1f1/Assets/luanPart/Script/PlayerRunHandle.cs
+++ AFE u5.4.1f1/Assets/luanPart/Script/PlayerRunHandle.cs
@@ -3,14 +3,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UniRx;
+using Photon.Pun;
 
-public class PlayerRunHandle : MonoBehaviour
+public class PlayerRunHandle : MonoBehaviourPun,
+    IInitialize<IChampionConfig>
 {
     private IJoystickInputFilterObserver joystickInputFilterObserver;
+
+    private IChampionConfig ChampionConfig { get; set; }
 
     private void Awake()
     {
         joystickInputFilterObserver = GetComponent<JoystickInputFilter>();
+    }
+
+    void IInitialize<IChampionConfig>.Initialize(IChampionConfig init)
+    {
+        ChampionConfig = init;
     }
 
     // Use this for initialization
@@ -19,7 +28,7 @@ public class PlayerRunHandle : MonoBehaviour
         joystickInputFilterObserver.OnRunAsObservable()
             .Subscribe(message =>
             {
-                transform.position = message.Direction;
+                transform.position = message.Direction * ChampionConfig.MoveSpeed.Value;
                 if (message.Rotation != Vector3.zero)
                     transform.rotation = Quaternion.LookRotation(message.Rotation);
             });
