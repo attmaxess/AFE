8e0d2d06d2887dd4d859915e9688a9104a73f85f
DESKTOP-5D9G5PK\Asrock <thandh@beetsoft.com.vn>
2019-03-27 17:00:34 +0700

    start demo ground

---
+++ AFE u5.4.1f1/Assets/luanPart/Script/CanvasJoystickManagerMethods.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasJoystickManagerMethods : MonoBehaviour
+{
+    [ContextMenu("SetCanvasOn")]
+    public void SetCanvasOn()
+    {
+        CanvasJoystickManager.Singleton.canvas.gameObject.SetActive(true);
+    }
+
+    [ContextMenu("SetCanvasOff")]
+    public void SetCanvasOff()
+    {
+        CanvasJoystickManager.Singleton.canvas.gameObject.SetActive(false);
+    }
+
+    [ContextMenu("ToggleCanvas")]
+    public void ToggleCanvas()
+    {
+        CanvasJoystickManager.Singleton.canvas.gameObject.SetActive(!CanvasJoystickManager.Singleton.canvas.gameObject.activeSelf);
+    }
+    
+}
