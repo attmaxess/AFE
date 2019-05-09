257a99223425ebeacded8c2a6fabee00862274a8
DESKTOP-5D9G5PK\Asrock <thandh@beetsoft.com.vn>
2019-05-09 15:09:49 +0700

    add tribal

---
+++ AFE u5.4.1f1/Assets/tPart/GamePlay/Scripts/ReadyToPlay/CanvasTribalWorld.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasTribalWorld : MonoBehaviour
+{
+    [ContextMenu("Zoom")]
+    public void Zoom()
+    {
+
+    }
+}
