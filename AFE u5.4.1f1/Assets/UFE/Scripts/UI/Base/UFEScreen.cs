9bf176f57bc8fcacb123330225a3943b3f53dd40
DESKTOP-5D9G5PK\Asrock <thandh@beetsoft.com.vn>
2019-05-10 11:42:17 +0700

    build test UI Tribal

--- AFE u5.4.1f1/Assets/UFE/Scripts/UI/Base/UFEScreen.cs
+++ AFE u5.4.1f1/Assets/UFE/Scripts/UI/Base/UFEScreen.cs
@@ -1,22 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class UFEScreen : MonoBehaviour{
+public class UFEScreen : MonoBehaviour
+{
 
     public bool canvasPreview = true;
     //public bool enableUFEInput = false;
-	public GameObject firstSelectableGameObject = null;
-	public bool wrapInput = true;
+    public GameObject firstSelectableGameObject = null;
+    public bool wrapInput = true;
 
-	public virtual void DoFixedUpdate(){}
+    public virtual void DoFixedUpdate() { }
 
-	public virtual bool IsVisible(){
-		return this.gameObject.activeInHierarchy;
-	}
+    public virtual bool IsVisible()
+    {
+        return this.gameObject.activeInHierarchy;
+    }
 
-	public virtual void OnHide(){}
-	public virtual void OnShow(){
+    public virtual void OnHide() { }
+    public virtual void OnShow()
+    {
         //UFE.PauseGame(!enableUFEInput);
     }
-	public virtual void SelectOption(int option, int player){}
+    public virtual void SelectOption(int option, int player) { }
 }
